4c1c0d5287ec82c079baea84ccce916bf203b4a3
Michael Costa <macosta@inf.ufpel.edu.br>
2019-05-29 21:57:48 -0300

    added saga on project
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import saga from "./Dashboard/saga";
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(saga);
 
 ReactDOM.render(
   <Provider store={store}>
