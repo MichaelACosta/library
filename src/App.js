10f6c0ae0b725fc96daf09d8b4d89800a55e503d
Michael Costa <macosta@inf.ufpel.edu.br>
2019-06-04 22:39:26 -0300

    app: added router
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Dashboard from "./Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => (
   <div className="App">
-    <Header />
-    <Dashboard />
+    <Router>
+      <Header />
+      <div>
+        <Route exact path="/" component={Dashboard} />
+      </div>
+    </Router>
   </div>
 );
 
