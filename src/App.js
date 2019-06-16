7b264d8f495d50e866b3ebc14cb9a35868f0a40b
Michael Costa <macosta@inf.ufpel.edu.br>
2019-06-16 15:31:52 -0300

    src: fixed to use router
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Dashboard from "./Dashboard";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => (
   <div className="App">
     <Router>
       <Header />
-      <div>
-        <Route exact path="/" component={Dashboard} />
-      </div>
+      <Dashboard />
     </Router>
   </div>
 );
